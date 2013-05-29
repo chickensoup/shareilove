bf71747dec1cdb9d951bf7961a71841d14bd0544
daodao <daodao@ubuntu.(none)>
2013-05-28 19:44:26 -0700

    example

--- example/settings.py
+++ example/settings.py
@@ -2,7 +2,7 @@
 SOCIALOAUTH_SITES = {
     'renren': ('socialoauth.sites.renren.RenRen',
                {
-                'redirect_uri': 'http://shareilove.org',
+                'redirect_uri': 'http://shareilove.org/account/oauth/renren',
                 'client_id': '1797aa8579004e2cbf04b25136871a6a',
                 'client_secret': 'c98f7849bf8648d298a3914fb3ad289a',
                 'scope': ['publish_feed', 'status_update']
@@ -11,7 +11,7 @@ SOCIALOAUTH_SITES = {
         
     'weibo': ('socialoauth.sites.weibo.Weibo',
               {
-                'redirect_uri': 'http://test.org/account/oauth/weibo',
+                'redirect_uri': 'http://shareilove.org/account/oauth/weibo',
                 'client_id': 'YOUR ID',
                 'client_secret': 'YOUR SECRET',
               }
@@ -19,9 +19,9 @@ SOCIALOAUTH_SITES = {
     
     'qq': ('socialoauth.sites.qq.QQ',
               {
-                'redirect_uri': 'http://test.org/account/oauth/qq',
-                'client_id': 'YOUR ID',
-                'client_secret': 'YOUR SECRET',
+                'redirect_uri': 'http://shareilove.org/account/oauth/qq',
+                'client_id': '100455018',
+                'client_secret': '3b805601e479c02c10750ab16ed7026c',
               }
     )    
 }
